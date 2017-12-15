de5a0a3b8975e45ec132f57783ada3b22aec2140
Meodo <follet.nico@gmail.com>
2017-12-15 13:29:21 +0100

    modif du dépot de l'image debian

--- DirtyCowDeb-8-0-0.sh
+++ DirtyCowDeb-8-0-0.sh
@@ -1,3 +1,3 @@
 rm -rf DirtyCowDeb-8-0-0
 mkdir DirtyCowDeb-8-0-0
-git clone https://github.com/Meodo/Securite-dirtyCow-Debian-8-0-0.git DirtyCowDeb-8-0-0
+xdg-open https://drive.google.com/file/d/1w_r0SB28bTK1h9U6Y7t4azgd9Qb7Sanm/view
